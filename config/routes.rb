9a343a58287243167419640a1cd8f34ccc59029c
HiroRin753 <chikuzen.0306@gmail.com>
2020-12-01 17:03:24 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
   root to: 'houses#index'
+  resources :houses 
+  resources :users, [:show]
+
+  get '/auth/:provider/callback', to: 'sessions#create' #ログイン認証
+  get '/logout', to: 'sessions#destroy' #ログアウト用
+  get "/auth/failure", to: "sessions#failure" #認証失敗時用
 end
